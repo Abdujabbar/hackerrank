8544651799980a35970cb847878adddf981ee9a5
Abdujabbor Mirxalikov <abdujabbor1987@gmail.com>
2016-07-12 01:51:25 +0500

    modifiers

---
+++ ds/stacks/waiter.py
@@ -0,0 +1,53 @@
+__author__ = 'abdujabbor'
+
+
+def is_prime(n):
+    if n == 2 or n == 3 or n == 5:
+        return True
+
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i =  5
+    w = 2
+    while i * i < n:
+        if n % i == 0:
+            return False
+
+        i += w
+        w = 6 - w
+
+    return True
+
+n, q = [int(x) for x in input().split()]
+pile = [int(x) for x in input().split()]
+
+t = 2
+stack = [t]
+counter = 1
+
+while counter < q:
+    t += 1
+    if is_prime(t):
+        stack.append(t)
+        counter += 1
+primes = []
+while stack:
+    v = stack.pop(0)
+    non_primes = []
+
+    while pile:
+        x = pile.pop()
+        if x % v == 0:
+            primes.append(x)
+        else:
+            non_primes.append(x)
+
+    while primes:
+        print(primes.pop(0))
+
+    pile = non_primes
+
+while non_primes:
+    print(non_primes.pop(0))
+
